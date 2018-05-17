1339e127a7f8dedd84d74760d948f838abfe1b8b
Ashinzekene <ashinzekene@gmail.com>
2018-05-17 08:24:34 +0100

    example works, angular6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public title: string = 'app';
+  public results = {
+    name: ''
+  }
+  tRef: string = ""
+  result = ""
+  constructor() {}
+
+  paymentDone(ref) {
+    this.title = "Payment successfull"
+    console.log(this.title)
+  }
+
+  paymentCancel() {
+    this.title = "Payment failed"
+    console.log(this.title)
+  }
+
+  ngOnInit() {
+    this.tRef = `${Math.random() * 10000000000000}`
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Angular4PaystackComponent } from 'projects/angular4-paystack/src/public_api';
+import { Angular4PaystackModule } from 'angular4-paystack';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular4PaystackModule
   ],
   providers: [],
   bootstrap: [AppComponent]
